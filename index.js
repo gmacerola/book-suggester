ff3a931a27b2acaa27c37fdb6f751c24437eeae6
Gianpaolo Macerola <gmacerola@gmail.com>
2020-08-13 05:39:49 -0500

    testing api for cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 let twitterTrends = [];
 let topNews = [];
+let numOfCompleted = 0;
 
 function getTrends() {
   var myHeaders = new Headers();
@@ -30,33 +31,62 @@ function getTrends() {
 }
 
 function topFive(responseJson) {
+  $("#results").html("<h2>Loading Twitter Trends...</h2>");
   for (let i = 0; i < 5; i++) {
+    $("#results").append(`<p>${responseJson[0].trends[i].name}</p>`);
     twitterTrends.push(responseJson[0].trends[i].name);
+    getNews(responseJson[0].trends[i].name.replace("#", ""), i);
   }
-  getNews();
 }
 
-function getNews() {
-  console.log(twitterTrends);
-  for (let i = 0; i < 5; i++) {
-    fetch(
-      `https://google-news.p.rapidapi.com/v1/topic_headlines?country=US&lang=en&topic=${twitterTrends[i]}`,
-      {
-        method: "GET",
-        headers: {
-          "x-rapidapi-host": "google-news.p.rapidapi.com",
-          "x-rapidapi-key":
-            "af2f355581msh9a32bc4ec2970e5p15799djsn5c391589925d",
-        },
-      }
-    )
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+function getNews(topic, index) {
+  // make an api call w/ that topic
+  // push those news into the array of news
+  fetch(
+    `http://newsapi.org/v2/everything?q=${topic}&apiKey=4963ba8ddaed4ec193d5b70fe3939b82&pageSize=5`
+  )
+    /*fetch(
+    `https://google-news.p.rapidapi.com/v1/search?country=US&to=5&lang=en&q=${topic}`,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "google-news.p.rapidapi.com",
+        "x-rapidapi-key": "af2f355581msh9a32bc4ec2970e5p15799djsn5c391589925d",
+      },
+    }
+  )*/
+    .then((response) => response.json())
+    .then((result) => {
+      topNews[index] = result.articles.slice(0, 5);
+      checkNewsDone();
+    }) // topNews [[article1,2,3,4,5],[1,2,3,4,5]]
+    .catch((error) => console.log("error", error));
+}
+
+function checkNewsDone() {
+  numOfCompleted++;
+  $("#results").append(`<p>Loading ${numOfCompleted} of 5</p>`);
+  if (numOfCompleted === 5) {
+    renderData();
   }
+}
+
+function renderData() {
+  // twitterTrends
+
+  for (let i = 0; i < twitterTrends.length; i++) {
+    let html = "<section class='news'>";
+    html += `<h2>${twitterTrends[i]}</h2>`;
+    html += "<ul class='articles'>";
+    for (let j = 0; j < topNews[i].length; j++) {
+      html += `<li>${topNews[i][j].title}</li>`;
+    }
+    html += "</ul></section>";
+    $("#results").html(html);
+  }
+
+  console.log();
+  // newsArray
 }
 
 getTrends();
