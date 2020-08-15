df3c305b19ccf08049767a4dc21af4b1c41aeb68
Gianpaolo Macerola <gmacerola@gmail.com>
2020-08-15 14:25:15 -0500

    keep getting undefined error

--- index.js
+++ index.js
@@ -35,17 +35,20 @@ function topFive(responseJson) {
   for (let i = 0; i < 5; i++) {
     $("#results").append(`<p>${responseJson[0].trends[i].name}</p>`);
     twitterTrends.push(responseJson[0].trends[i].name);
-    getNews(responseJson[0].trends[i].name.replace("#", ""), i);
+    setTimeout(
+      () => getNews(responseJson[0].trends[i].name.replace("#", ""), i),
+      1000
+    );
   }
 }
 
 function getNews(topic, index) {
   // make an api call w/ that topic
   // push those news into the array of news
-  fetch(
+  /*fetch(
     `https://newsapi.org/v2/everything?q=${topic}&apiKey=4963ba8ddaed4ec193d5b70fe3939b82&pageSize=5`
-  )
-    /*fetch(
+  )*/
+  /*fetch(
     `https://google-news.p.rapidapi.com/v1/search?country=US&to=5&lang=en&q=${topic}`,
     {
       method: "GET",
@@ -55,9 +58,28 @@ function getNews(topic, index) {
       },
     }
   )*/
+  /*fetch(
+    `https://gnews.io/api/v3/search?q=${topic}&max=2&token=c4f50db60162137467804706598b52f1`
+  )*/
+  var myHeaders = new Headers();
+  myHeaders.append(
+    "Ocp-Apim-Subscription-Key",
+    "fae2d3935f5847c08503ac19fe08aa52"
+  );
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+  fetch(
+    `https://api.cognitive.microsoft.com/bing/v7.0/news/search?q=${topic}&count=5`,
+    requestOptions
+  )
     .then((response) => response.json())
     .then((result) => {
-      topNews[index] = result.articles.slice(0, 5);
+      topNews[index] = result.value;
+      //topNews[index] = result.value.slice(0, 5);
       checkNewsDone();
     }) // topNews [[article1,2,3,4,5],[1,2,3,4,5]]
     .catch((error) => console.log("error", error));
@@ -73,20 +95,27 @@ function checkNewsDone() {
 
 function renderData() {
   // twitterTrends
-
+  let html = "";
   for (let i = 0; i < twitterTrends.length; i++) {
-    let html = "<section class='news'>";
+    html += "<section class='news'>";
     html += `<h2>${twitterTrends[i]}</h2>`;
     html += "<ul class='articles'>";
     for (let j = 0; j < topNews[i].length; j++) {
-      html += `<li>${topNews[i][j].title}</li>`;
+      html += `<li>
+        
+        <h3>${topNews[i][j].name}</h3>
+        <p>${topNews[i][j].description}</p>
+      </li>`;
     }
     html += "</ul></section>";
-    $("#results").html(html);
   }
+
+  $("#results").html(html);
 
   console.log();
   // newsArray
 }
 
 getTrends();
+
+//<div class="thumbnail" style="background-image:url('${topNews[i][j].image.thumbnail.contentUrl}')"></div>
